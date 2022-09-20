efe95d7bb668691b8f0f4b92df8fbbc9104ff0fe
BT_4N2_06 <davidn166@nycstudents.net>
2022-09-20 12:00:45 -0400

    Name Added

--- src/Sample.java
+++ src/Sample.java
@@ -1,6 +1,7 @@
 public class Sample {
     public static void main(String[] args) {
-        System.out.print("Now you see me");
+        System.out.println("David Ni");
+        System.out.println("Now you see me");
 
     }
 }
